ce62d8dfe5c8a37573eecf697c96bf1c6b3fc9b6
nelli her <nellih1117@gmail.com>
2021-04-15 15:33:07 -0700

    finish exam-end

--- test_1.py
+++ test_1.py
@@ -14,3 +14,55 @@ if the amount is equal, we output to the user "Option 1 and Option 2 pays the sa
 if option1 is better, we output to the user "Option 1 is better"
 if option2 is better, we output to the user "Option 2 is better" 
 """
+
+"""
+# option1
+  return 100 *10
+
+#option2
+  amount = 1
+  list1 = []
+  loop 10 times
+    add amount to list1
+    amount *=2
+  sum = sum of all items in loop
+  return sum
+# main
+  var1 = option1
+  var2 = option2
+
+  if var1 = var2
+    "Option 1 and Option 2 pays the same"
+  if var1 < var2
+    "Option 2 is better"
+  else
+    "Option 1 is better" 
+
+main
+"""
+def option1():
+  return 100 * 10
+
+def option2():
+  amount = 1
+  list1 = []
+  for i in range(0,10):
+    list1.append(amount)
+    amount *=2
+  print("list1", list1)
+  total = sum(list1) 
+  return total
+
+def main():
+  answer = ""
+  var1 = option1() # 1000
+  var2 = option2() # 1023
+  if var1 == var2: 
+    answer = "Option 1 and Option 2 pays the same"
+  elif var1 < var2:
+    answer = "Option 2 is better"
+  else:
+    answer = "Option 1 is better" 
+  print(answer)
+
+main()
